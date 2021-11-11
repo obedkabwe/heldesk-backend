0fc37b10de28c3540c64fa506918e309e09916e0
kabweobeed <kabweobed@gmail.com>
2021-11-10 21:29:08 -0300

    anotacao jpa para classe de dominio

--- src/main/java/com/obed/helpdesk/domain/Chamado.java
+++ src/main/java/com/obed/helpdesk/domain/Chamado.java
@@ -1,21 +1,44 @@
 package com.obed.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.obed.helpdesk.domain.enums.Prioridade;
 import com.obed.helpdesk.domain.enums.Status;
 
-public class Chamado {
-
+@Entity
+public class Chamado implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private LocalDate dataAbertura = LocalDate.now();
-	private LocalDate dataFechamento =LocalDate.now();
+	
+    @JsonFormat(pattern = "dd/MM/yyy")
+    private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyy")
+    private LocalDate dataFechamento =LocalDate.now();
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacaoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
+	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	@Override
 	public int hashCode() {
@@ -101,7 +124,6 @@ public class Chamado {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	private Tecnico tecnico;
 	
 	
 	
